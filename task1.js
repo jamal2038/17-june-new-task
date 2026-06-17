e0a83037561b970b4a8d0eb302e18042510e97e1
jamal2038 <jamalsoomro2245@gmail.com>
2026-06-17 15:49:58 +0500

    first commit

---
+++ task1.js
@@ -0,0 +1,42 @@
+let month=prompt("enter any month name")
+
+switch(month){
+    case '1':
+        console.log("january")
+        break;
+    case '2':
+        console.log("febewary")
+        break;
+    case '3':
+        console.log("march")
+        break;
+    case '4':
+        console.log("aprail")
+        break;
+    case '5':
+        console.log("may")
+        break;
+    case'6':
+        console.log("june")
+        break;
+    case'7':
+        console.log("julay")
+        break;
+    case '8':
+        console.log("augest")
+        break;
+    case '9':
+        console.log("septumber")
+        break;
+    case '10':
+        console.log("octumber")
+        break;
+    case '11':
+        console.log("november")
+        break;
+    case '12':
+        console.log("december")
+        break;
+    default:
+        console.log("invaild value")
+}
