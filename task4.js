a4eccc0ffe5fb259d7eefac58bfd714d7ed9b8b8
jamal2038 <jamalsoomro2245@gmail.com>
2026-06-17 16:13:37 +0500

    3rd commit

---
+++ task4.js
@@ -0,0 +1,18 @@
+let color = prompt("Enter traffic light color (Red, Yellow, Green):");
+
+switch (color.toLowerCase()) {
+    case "red":
+        console.log("Stop");
+        break;
+
+    case "yellow":
+        console.log("Get Ready");
+        break;
+
+    case "green":
+        console.log("Go");
+        break;
+
+    default:
+        console.log("Invalid Color");
+}
